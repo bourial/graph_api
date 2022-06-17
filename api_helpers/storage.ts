cf95074d2982b000d291bb9505c05fd933b65346
abdo <abderrazakbourial0@gmail.com>
2022-06-17 10:42:30 +0100

    test

--- api_helpers/storage.ts
+++ api_helpers/storage.ts
@@ -1,13 +1,13 @@
 import fs from "fs";
 
 export const setPage = (pageId: string, pageToken: string) => {
-  fs.writeFileSync("../page.json", JSON.stringify({ pageId, pageToken }));
+  fs.writeFileSync("page.json", JSON.stringify({ pageId, pageToken }));
 };
 
 export const getPage = () => {
   console.log("getting storage");
 
-  const page = fs.readFileSync("../page.json", "utf-8");
+  const page = fs.readFileSync("page.json", "utf-8");
   console.log("page", page);
 
   return JSON.parse(page) as { pageId: string; pageToken: string };
